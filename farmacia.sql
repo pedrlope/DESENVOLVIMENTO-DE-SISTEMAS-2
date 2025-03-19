dcb5956b48c3970c51c4d01bb8521b6a0ead8fb8
pedrlope <edroplope@gmail.com>
2025-03-19 19:41:12 -0300

    Update farmacia.sql

--- farmacia.sql
+++ farmacia.sql
@@ -21,3 +21,27 @@ FOREIGN KEY (ID_produto) REFERENCES produtos(ID_produto)
 select * from clientes;
 
 select * from produtos;
+
+C#  private void button2_Click(object sender, EventArgs e)
+{
+    try
+    {
+        //crio o comando de conexao com o banco de dados passando o local, usuario e senha
+        string data_source = "datasource=localhost;username=root;password='';database=farmacia";
+        conexao = new MySqlConnection(data_source);//crio uma nova conexao
+        //crio o script sql de inserir as informações
+        string sql2 = "DELETE FROM CLIENTES WHERE ID_cliente = 2;";
+        MySqlCommand command = new MySqlCommand(sql2, conexao);
+
+        conexao.Open();//abrir meu banco de dados
+        command.ExecuteReader();//executa a consulta ou comando
+        MessageBox.Show("Deletado!");
+    }
+    catch (Exception ex)
+    {//qual mensagemde erro
+        MessageBox.Show(ex.Message);
+    }
+    finally
+    {
+        conexao.Close();//fechar a conexao do banco
+    }
